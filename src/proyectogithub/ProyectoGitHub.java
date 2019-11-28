cf2bb7f174b1bbb4fe471ca8a7cc6ff8aedcc457
Andres <trevvieaamb@gmail.com>
2019-11-28 10:04:03 +0100

    Inicio

---
+++ src/proyectogithub/ProyectoGitHub.java
@@ -0,0 +1,20 @@
+
+package proyectogithub;
+
+public class ProyectoGitHub {
+
+    public static String veredicto(String valor1, String valor2){
+        if (valor1.equals(valor2)) {
+            return "Perfecto. Has tenido buen criterio.";
+        }
+        if (Integer.valueOf(valor1) < Integer.valueOf(valor2)){
+            return "Has mejorado, o te ha llegado el éxito inesperado.";
+        }
+        return "Te has confiado. Falta realismo.";
+    }
+    
+    public static void main(String[] args) {
+        System.out.println("La calificación de " + args[0] + ": "+args[2]+"\n"+veredicto(args[1], args[2]));
+    }
+    
+}
